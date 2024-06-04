532fe75905e2534577a23f38cad8a42354af5455
CarolayneUriel <carolurielmarques@gmail.com>
2024-06-04 00:56:24 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrilhaTesteDesafio
+{
+    internal class Calculadora
+    {
+        private List<string> listahistorico;
+
+        public Calculadora()
+        {
+            listahistorico = new List<string>();
+        }
+
+        public int Somar(int num1, int num2)
+        {
+            int result = num1 + num2;
+            listahistorico.Insert(0, "Res" + result);
+            return result;
+        }
+
+        public int Subtrair(int num1, int num2)
+        {
+            int result = num1 - num2;
+            listahistorico.Insert(0, "Res" + result);
+            return result;
+        }
+
+        public int Multiplicar(int num1, int num2)
+        {
+            int result = num1 * num2;
+            listahistorico.Insert(0, "Res" + result);
+            return result;
+        }
+
+        public int Dividir(int num1, int num2)
+        {
+            int result = num1 / num2;
+            listahistorico.Insert(0, "Res" + result);
+            return result;
+        }
+
+        public List<string> historico()
+        {
+            listahistorico.RemoveRange(3, listahistorico.Count - 3);
+            return listahistorico;
+        }
+
+    }
+}
